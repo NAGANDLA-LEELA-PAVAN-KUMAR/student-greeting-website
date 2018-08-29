d817aecd1fc401b6ffd0e4647c791f4bd261a80d
NAGANDLA-LEELA-PAVAN-KUMAR <33209739+NAGANDLA-LEELA-PAVAN-KUMAR@users.noreply.github.com>
2018-08-30 00:28:56 +0530

    Add files via upload

---
+++ dbconn/dbconn.php
@@ -0,0 +1,20 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try 
+	{
+    $conn = new PDO("mysql:host=$servername;dbname=dob", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    //echo "Connection failed: " . $e->getMessage();
+    }
+?>
+<!--BOOSTRAP FILES-->
+<link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css">
+<script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
